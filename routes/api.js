66ff941393200235becf0d50d0cd08bc565bdcec
Adam Bowman <adamcbowman@gmail.com>
2022-05-30 13:06:41 -0300

    move data to gist file and edit index page

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const axios = require("axios");
 
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/data/resume.json'));
+const getResume = async () => {
+  try {
+    const data = await axios.get(
+      "https://api.github.com/gists/5e8846d20351f7d5ab004bc692b00262"
+    );
+    const resume = await data.data.files["resume.json"].content;
+    return JSON.parse(resume);
+  } catch (error) {
+    return error;
+  }
+};
+
+router.get("/", function (req, res, next) {
+  const resume = getResume();
+  resume.then((data) => {
+    res.json(data);
+  });
 });
 
 module.exports = router;
